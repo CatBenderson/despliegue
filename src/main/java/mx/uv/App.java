02a022609b99e4bfa17549a5cf95e14c304667ec
CatBenderson <112489966+CatBenderson@users.noreply.github.com>
2022-12-09 17:57:45 -0600

    Update App.java

--- src/main/java/mx/uv/App.java
+++ src/main/java/mx/uv/App.java
@@ -69,13 +69,14 @@ public class App {
             objetoJson.addProperty("status", false);
             return objetoJson;
         });
-    }
-
-    static int getHerokuAssignedPort() {
+        
+        static int getHerokuAssignedPort() {
         ProcessBuilder processBuilder = new ProcessBuilder();
         if (processBuilder.environment().get("PORT") != null) {
             return Integer.parseInt(processBuilder.environment().get("PORT"));
         }
         return 4567; // return default port if heroku-port isn't set (i.e. on localhost)
+    }
+        
     }
 }
